07ae417a0b5367a66da74c5d3431fffe303ff068
Kysophy <khoas8232@gmail.com>
2025-06-30 13:09:35 +0700

    Delete cms directory

--- cms/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.medisys {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires transitive javafx.graphics;// I add this but not really nesessary
-    opens com.medisys to javafx.fxml;
-    exports com.medisys;
-}
