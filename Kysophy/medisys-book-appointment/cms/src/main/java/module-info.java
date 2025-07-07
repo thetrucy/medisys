80546b90245566638cd966f991e32cabde6df4ed
Kysophy <khoas8232@gmail.com>
2025-07-07 23:53:42 +0700

    Delete Kysophy/medisys-book-appointment directory

--- Kysophy/medisys-book-appointment/cms/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.medisys {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires transitive javafx.graphics;
-	requires javafx.base;// I add this but not really nesessary
-    opens com.medisys to javafx.fxml;
-    exports com.medisys;
-}
